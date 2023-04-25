32c834a75d7c1def7778dff2cab5472fb29f1c44
klandestino7 <klandestino6@hotmail.com>
2023-04-25 17:49:41 -0300

    update functions to inventory

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,8 @@
+
+local Items = exports.inventory:ItemsList()
+
 local ShopItems = {}
 
-data = {}
-TriggerEvent("redemrp_inventory:getData",function(call)
-    data = call
-end)
 
 Citizen.CreateThread(function()
     LoadShop()
@@ -11,14 +10,19 @@ end)
 
 function LoadShop()
     ShopItems = {}
-   for k,v in pairs(ShopItemsData) do
+
+    for k,v in pairs(ShopItemsData) do
         if ShopItems[v.store] == nil then
             ShopItems[v.store] = {}
         end
+
         local name = v.item
-        if data.getItemData(name) ~= nil and data.getItemData(name).label ~= nil then
-            name = data.getItemData(name).label
+
+        local item = Items[name]
+        if item and item.label then
+            name = item.label
         end
+
         table.insert(ShopItems[v.store], {
             label = name,
             item  = v.item,
@@ -32,11 +36,12 @@ end
 function GetPrice(ItemName , Zone)
 	for k,v in pairs(ShopItems[Zone]) do
         if v.item == ItemName then
-           return v.price
+            return v.price
         end
-   end
-   return 0
+    end
+    return 0
 end
+
 RegisterServerEvent('redemrp_shops:RequestItems')
 AddEventHandler('redemrp_shops:RequestItems', function()
     local _source = source
@@ -47,31 +52,35 @@ RegisterServerEvent('redemrp_shops:BuyItem')
 AddEventHandler('redemrp_shops:BuyItem', function(itemName, amount , zone)
     local _source = source
     local amount_ = math.floor(amount)
+
 	if amount_ < 0 then return end
+
 	local ItemPrice = GetPrice(itemName , zone)
 	local TotalPrice = ItemPrice * amount_
-    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
 
+    local Item = Items[itemName]
+
+    TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
             if user.getMoney() >= TotalPrice then
 
-                local ItemData = data.getItem(_source, itemName)
-                if not ItemData.AddItem(amount_) then
+                if not exports.inventory:canCarryItem(_source, itemName, amount) then
                     TriggerClientEvent("redemrp_notification:start", _source, "You don't have enough space!", 3, "error")
                 else
-                    user.removeMoney(TotalPrice)
+                    if exports.inventory:AddItem(_source, itemName, amount) then
+                        user.removeMoney(TotalPrice)
 
-                    local name = itemName
-                    if ItemData.ItemInfo.label ~= nil then
-                        name = ItemData.ItemInfo.label
+                        local name = Item.label
+
+                        TriggerClientEvent("pNotify:SendNotification", _source , {
+                            text = "<img src='nui://redemrp_inventory/html/items/"..itemName..".png' height='40' width='40' style='float:left; margin-bottom:10px; margin-left:20px;' />You Bought: ".. name.."<br>+"..tonumber(amount_),
+                            type = "success",
+                            timeout = math.random(2000, 3000),
+                            layout = "centerLeft",
+                            queue = "right"
+                        })
                     end
-                    TriggerClientEvent("pNotify:SendNotification", _source , {
-                        text = "<img src='nui://redemrp_inventory/html/items/"..itemName..".png' height='40' width='40' style='float:left; margin-bottom:10px; margin-left:20px;' />You Bought: ".. name.."<br>+"..tonumber(amount_),
-                        type = "success",
-                        timeout = math.random(2000, 3000),
-                        layout = "centerLeft",
-                        queue = "right"
-                    })
                 end
+
             else
                 local missingMoney = TotalPrice - user.getMoney()
                 TriggerClientEvent("redemrp_notification:start", _source, "You don't have enough money: "..missingMoney, 3, "error")
